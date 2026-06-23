e747157415ffca602112013b2602b9959e1b237c
H2 <h.mwondi@gmail.com>
2026-06-23 17:35:24 -0400

    Fix : tests config fix

--- src/test/java/com/Ledger_API/Ledger_API/LedgerApiApplicationTests.java
+++ src/test/java/com/Ledger_API/Ledger_API/LedgerApiApplicationTests.java
@@ -2,7 +2,9 @@ package com.Ledger_API.Ledger_API;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class LedgerApiApplicationTests {
 
